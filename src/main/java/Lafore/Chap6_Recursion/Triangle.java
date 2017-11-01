2d1b46a88ad5b3e9df5664ccb936a370ab57d72a
Elena Dolgova <levinforce@gmail.com>
2017-11-02 00:11:38 +0300

    Add recursive triangle

---
+++ src/main/java/Lafore/Chap6_Recursion/Triangle.java
@@ -0,0 +1,21 @@
+package Lafore.Chap6_Recursion;
+
+public class Triangle {
+
+    public long triangle(long n){
+        long total = 0;
+        while(n>0){
+            total+=n;
+            n--;
+        }
+        return total;
+    }
+
+    public long triangleRec(long n){
+        if(n==1){
+            return 1;
+        }else {
+            return triangleRec(n-1)+n;
+        }
+    }
+}
---
+++ src/test/java/Lafore/Chap6_Recursion/Triangle_Test.java
@@ -0,0 +1,27 @@
+package Lafore.Chap6_Recursion;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class Triangle_Test {
+
+    Triangle triangle = new Triangle();
+
+    @Test
+    public void triangleRec_Test(){
+
+        assertEquals(1,triangle.triangle(1));
+        assertEquals(3,triangle.triangle(2));
+        assertEquals(6,triangle.triangle(3));
+        assertEquals(10,triangle.triangle(4));
+        assertEquals(15,triangle.triangle(5));
+
+
+        assertEquals(1,triangle.triangleRec(1));
+        assertEquals(3,triangle.triangleRec(2));
+        assertEquals(6,triangle.triangleRec(3));
+        assertEquals(10,triangle.triangleRec(4));
+        assertEquals(15,triangle.triangleRec(5));
+    }
+}
